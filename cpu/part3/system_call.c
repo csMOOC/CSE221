c0143fcae3493f2490283b501fd9b26bbb79dc07
Louis1992 <303542123@qq.com>
2015-12-23 22:50:09 -0800

    finish 1.3

---
+++ cpu/part3/system_call.c
@@ -0,0 +1,28 @@
+#include "rdtsc.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    
+    if(argc < 2) {
+        printf("usage : ./program 1000000");
+        exit(0);
+    }
+    
+    int loops = atoi(argv[1]);
+
+    int i = 0;
+    unsigned long long total = 0;
+    unsigned long long start, end;
+    start = rdtsc();
+    for(;i < loops;++i) {
+        getppid();
+    }
+    end = rdtsc();
+    total = end - start;
+    printf("System call overhead is : %llu cycles; Total : %llu \n", total/loops, total);
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: cpu
 .PHONY:all
 
-cpu : cpu1 cpu2
+cpu : cpu1 cpu2 cpu3
 
 cpu1 : 
 	gcc -Iinclude -o cpu1.1.out cpu/part1/cpuread.c;mv cpu1.1.out build
@@ -10,6 +10,8 @@ cpu1 :
 cpu2 : 
 	gcc -Iinclude -o cpu2.1.out cpu/part2/procedure_call.c;mv cpu2.1.out build
 
+cpu3 : 
+	gcc -Iinclude -o cpu3.1.out cpu/part3/system_call.c;mv cpu3.1.out build
 
 
 clean :
