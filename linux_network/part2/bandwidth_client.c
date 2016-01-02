ffd8528c3d213fb1689cc0aaffc42f2c95a58b67
Louis1992 <303542123@qq.com>
2016-01-01 21:56:43 -0800

    3.2 code

---
+++ linux_network/part2/bandwidth_client.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "rdtsc.h"
+
+int main(int argc , char *argv[])
+{      
+	if(argc != 4) {
+		perror("usage : ./client ip port size \n");
+		return -1;
+	}
+
+	int port = atoi(argv[2]);
+    int size = atoi(argv[3]);
+
+    struct sockaddr_in server;
+    server.sin_addr.s_addr =inet_addr(argv[1]);
+    server.sin_family = AF_INET;
+    server.sin_port = htons( port );
+    int sockfd;
+    
+    // Create socket
+    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+    if (sockfd == -1) {
+        perror("Error: fails to create socket");
+		return -1;
+	}
+
+	if (connect(sockfd,(struct sockaddr *)&server,sizeof(server)) < 0) {
+		perror("ERROR connecting");
+		return -1;
+	}
+
+    // Computing round trip time
+    char *msg = (char*)malloc(size);
+    unsigned long long start,end,diff;
+	int j = 0;
+
+    start = rdtsc();
+    recv(sockfd, &msg, size, MSG_WAITALL);
+    end = rdtsc();
+    diff = end - start;
+
+    close(sockfd);
+
+    printf ("PEAK bandwidth is : %f MB/s \n", (size/(1024*1.0*1024)) * (2.3e8 / diff) );
+    
+	return 0;
+}
---
+++ linux_network/part2/bandwidth_server.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+
+int main(int argc , char *argv[])
+{
+	if(argc != 3) {
+		perror("usage : ./server port size \n");
+		return -1;
+	}
+
+	int port = atoi(argv[1]);
+    int size = atoi(argv[2]);
+
+    int server_sock , client_sock , client_len;
+    struct sockaddr_in server , client;
+    
+    client_len = sizeof(struct sockaddr_in);
+    
+	//Create socket
+    server_sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (server_sock == -1) {
+        printf("Error: fails to create socket");
+		return -1;
+	}
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( port );
+     
+    //Bind
+    if(bind(server_sock,(struct sockaddr *)&server , sizeof(server)) < 0) {
+        perror("Error: fails to bind");
+        return 1;
+    }
+     
+    //Listen
+    listen(server_sock , 3);
+     
+    //Accept incoming connection
+    puts("Waiting   ...  \n");
+
+    char *msg=(char*)malloc(size);
+    memset (msg, 0, size);
+
+    //accept connection from an incoming client
+    while(1)
+    {
+    	client_sock = accept(server_sock, (struct sockaddr *)&client, (socklen_t*)&client_len);
+    	
+		if (client_sock < 0) {
+            perror("Error: fails to accept");
+            return -1;
+    	}
+
+        send(client_sock, &msg, size, 0);
+        
+        close(client_sock);
+    }
+    
+    close(server_sock);
+
+    puts("Finish calculating \n");
+     
+    return 0;
+}
